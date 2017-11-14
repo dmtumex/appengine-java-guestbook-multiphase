f79191eb8375bb5e6319b9c0edb2bab90beb8fbb
dmtumex <33655968+dmtumex@users.noreply.github.com>
2017-11-14 16:01:36 +0100

    delete oldest greeting if more than 5 exist

--- final/src/main/java/com/example/guestbook/SignGuestbookServlet.java
+++ final/src/main/java/com/example/guestbook/SignGuestbookServlet.java
@@ -17,22 +17,18 @@
 //[START all]
 package com.example.guestbook;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.googlecode.objectify.Key;
 import com.googlecode.objectify.ObjectifyService;
 
 /**
@@ -59,6 +55,11 @@ public class SignGuestbookServlet extends HttpServlet {
     } else {
       greeting = new Greeting(guestbookName, content);
     }
+
+    Key<Guestbook> theBook = Key.create(Guestbook.class, guestbookName);
+    List<Greeting> greetings = ObjectifyService.ofy().load().type(Greeting.class).ancestor(theBook).order("date").list();
+    if(greetings.size() >= 5)
+      ObjectifyService.ofy().delete().entity(greetings.get(0)).now();
 
     // Use Objectify to save the greeting and now() is used to make the call synchronously as we
     // will immediately get a new page using redirect and we want the data to be present.
